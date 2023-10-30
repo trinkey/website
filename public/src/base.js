e6e1e8d0730bebac54ea0539a96b6f178d6c4130
trinkey <97406176+trinkey@users.noreply.github.com>
2023-10-30 19:10:33 -0400

    v2.0.1

--- public/src/base.js
+++ public/src/base.js
@@ -2,31 +2,35 @@
 let updateTheme = () => { document.getElementsByTagName("body")[0].setAttribute("data-theme", localStorage.getItem("trinkey-theme") || "dark"); };
 updateTheme();
 
+enableBar = localStorage.getItem("trinkey-titlebar");
+if (enableBar === "1" || enableBar === undefined) { enableBar = true; }
+else { enableBar = false; }
+
 // Inject title bar
 titleBar = `<nav style="position: relative; z-index: 1000;">
   <table class="menu" id="fullDisplay">
     <tr>
-      <td class="logo"><div class="logo"><a class="plainLink" href="/">Trinkey's Website</a></div></td>
+      ${enableBar ? `<td class="logo"><div class="logo"><a class="plainLink" href="/">Trinkey's Website</a></div></td>` : ""}
       <td class="long"></td>
-      <td class="only-large"><a href="/about-me">About Me</a></td>
-      <td class="only-large"><a href="/photography">Photography</a></td>
-      <td class="only-large"><a href="/other">Other</a></td>
+      ${enableBar ? `<td class="only-large"><a href="/about-me">About Me</a></td>
+      <td class="only-large"><a href="/photography">Photography</a></td>` : ""}
       <td class="only-large"><a href="/games">Games</a></td>
-      <td class="only-large"><a href="/changelog">Changelog</a></td>
-      <td class="only-large light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'>Light Mode</td>
-      <td class="only-large dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'>Dark Mode</td>
+      <td class="only-large"><a href="/other">Other</a></td>
+      ${enableBar ? `<td class="only-large"><a href="/changelog">Changelog</a></td>` : ""}
+      <td class="only-large light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'><div>Light Mode</div></td>
+      <td class="only-large dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'><div>Dark Mode</div></td>
       <td class="only-small"><div class="hamburger" id="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div></td>
     </tr>
   </table>
 
   <ul class="noListIcon only-small hidden" id="hamburgerDisplay">
-    <li><a href="/about-me">About Me</a></td>
-    <li><a href="/photography">Photography</a></td>
-    <li><a href="/other">Other</a></td>
+  ${enableBar ? `<li><a href="/about-me">About Me</a></td>
+    <li><a href="/photography">Photography</a></td>` : ""}
     <li><a href="/games">Games</a></td>
-    <li><a href="/changelog">Changelog</a></td>
-    <li class="light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'>Light Mode</td>
-    <li class="dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'>Dark Mode</td>
+    <li><a href="/other">Other</a></td>
+    ${enableBar ? `<li><a href="/changelog">Changelog</a></td>` : ""}
+    <li class="light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'><div>Light Mode</div></td>
+    <li class="dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'><div>Dark Mode</div></td>
   </ul>
 </nav>`;
 
