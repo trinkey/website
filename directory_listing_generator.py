da2cc444d2051aeb614ed407f394a828cf78a161
trinkey <97406176+trinkey@users.noreply.github.com>
2023-11-06 21:58:39 -0500

    add favicon to the dir listings

--- directory_listing_generator.py
+++ directory_listing_generator.py
@@ -12,6 +12,7 @@ base = """
 
     <link rel="stylesheet" href="/css/base.css">
     <link rel="stylesheet" href="/css/directory.css">
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
   </head>
 
   <body>
