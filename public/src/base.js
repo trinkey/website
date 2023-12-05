8fe105c28a31406df210ed3ead53772c79894016
trinkey <97406176+trinkey@users.noreply.github.com>
2023-12-05 18:00:59 -0500

    small tweaks and url parameter changes
    not version worthy
    
    new url tweaks described in  /js/base.js

--- public/src/base.js
+++ public/src/base.js
@@ -1,46 +1,63 @@
+// Parse URL parameters
+// titlebar - titlebar (0 - enable titlebar, anything else: disable titlebar)
+// self - references to myself (0 - enable references, anything else: disable references)
+// redir - redirects to exact page
+// Example: //trinkey.web.app/?titlebar=1&self=1&redir=%2Fgames%2Fconnect4
+// this would disable the titlebar, disable all references, to me, and redirect to the connect four page.
+
+const urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.get("self")) { localStorage.setItem("trinkey-titlebar", urlParams.get("self")); }
+enableSelf = (localStorage.getItem("trinkey-titlebar") || "0") === "0"
+
+if (urlParams.get("titlebar")) { localStorage.setItem("trinkey-bar", urlParams.get("titlebar")); }
+enableTitlebar = (localStorage.getItem("trinkey-bar") || "0") === "0"
+
+if (urlParams.get("redir")) { window.location.href = urlParams.get("redir"); }
+
 // Set theme
 let updateTheme = () => { document.getElementsByTagName("body")[0].setAttribute("data-theme", localStorage.getItem("trinkey-theme") || "dark"); };
 let dom = (id) => ( document.getElementById(id) );
 
 updateTheme();
 
-enableBar = localStorage.getItem("trinkey-titlebar") === "0" ? false : true;
-
 // Inject title bar
-titleBar = `<nav style="position: relative; z-index: 1000;">
-  <table class="menu" id="fullDisplay">
-    <tr>
-      ${enableBar ? `<td class="logo"><div class="logo"><a class="plainLink" href="/">Trinkey's Website</a></div></td>` : ""}
-      <td class="long"></td>
-      ${enableBar ? `<td class="only-large"><a href="/about-me">About Me</a></td>
-      <td class="only-large"><a href="/photography">Photography</a></td>` : ""}
-      <td class="only-large"><a href="/games">Games</a></td>
-      <td class="only-large"><a href="/other">Other</a></td>
-      ${enableBar ? `<td class="only-large"><a href="/changelog">Changelog</a></td>` : ""}
-      <td class="only-large light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'><div>Light Mode</div></td>
-      <td class="only-large dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'><div>Dark Mode</div></td>
-      <td class="only-small"><div class="hamburger" id="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div></td>
-    </tr>
-  </table>
+if (enableTitlebar) {
+  titleBar = `<nav style="position: relative; z-index: 1000;">
+    <table class="menu" id="fullDisplay">
+      <tr>
+        ${enableSelf ? `<td class="logo"><div class="logo"><a class="plainLink" href="/">Trinkey's Website</a></div></td>` : ""}
+        <td class="long"></td>
+        ${enableSelf ? `<td class="only-large"><a href="/about-me/">About Me</a></td>
+        <td class="only-large"><a href="/photography/">Photography</a></td>` : ""}
+        <td class="only-large"><a href="/games/">Games</a></td>
+        <td class="only-large"><a href="/other/">Other</a></td>
+        ${enableSelf ? `<td class="only-large"><a href="/changelog/">Changelog</a></td>` : ""}
+        <td class="only-large light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'><div>Light Mode</div></td>
+        <td class="only-large dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'><div>Dark Mode</div></td>
+        <td class="only-small"><div class="hamburger" id="hamburger"><div class="bar"></div><div class="bar"></div><div class="bar"></div></div></td>
+      </tr>
+    </table>
 
-  <ul class="noListIcon only-small hidden" id="hamburgerDisplay">
-  ${enableBar ? `<li><a href="/about-me">About Me</a></td>
-    <li><a href="/photography">Photography</a></td>` : ""}
-    <li><a href="/games">Games</a></td>
-    <li><a href="/other">Other</a></td>
-    ${enableBar ? `<li><a href="/changelog">Changelog</a></td>` : ""}
-    <li class="light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'><div>Light Mode</div></td>
-    <li class="dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'><div>Dark Mode</div></td>
-  </ul>
-</nav>`;
+    <ul class="noListIcon only-small hidden" id="hamburgerDisplay">
+    ${enableSelf ? `<li><a href="/about-me/">About Me</a></td>
+      <li><a href="/photography/">Photography</a></td>` : ""}
+      <li><a href="/games/">Games</a></td>
+      <li><a href="/other/">Other</a></td>
+      ${enableSelf ? `<li><a href="/changelog/">Changelog</a></td>` : ""}
+      <li class="light-toggle" onclick='localStorage.setItem("trinkey-theme", "light"); updateTheme();'><div>Light Mode</div></td>
+      <li class="dark-toggle" onclick='localStorage.setItem("trinkey-theme", "dark"); updateTheme();'><div>Dark Mode</div></td>
+    </ul>
+  </nav>`;
 
-const elem1 = document.createElement("header");
-const elem2 = document.createElement("div");
-elem1.innerHTML = titleBar;
-elem2.innerHTML = '<div style="display: block; height: 62px; width: 1px; opacity: 0%;"></div>'
-document.getElementsByTagName("body")[0].prepend(elem1);
-document.getElementsByTagName("body")[0].prepend(elem2);
+  const elem1 = document.createElement("header");
+  const elem2 = document.createElement("div");
+  elem1.innerHTML = titleBar;
+  elem2.innerHTML = '<div style="display: block; height: 62px; width: 1px; opacity: 0%;"></div>';
+  document.getElementsByTagName("body")[0].prepend(elem1);
+  document.getElementsByTagName("body")[0].prepend(elem2);
 
-document.getElementById("hamburger").addEventListener("click", function() {
-  document.getElementById("hamburgerDisplay").classList.toggle("hidden");
-});
+  document.getElementById("hamburger").addEventListener("click", function () {
+    document.getElementById("hamburgerDisplay").classList.toggle("hidden");
+  });
+}
